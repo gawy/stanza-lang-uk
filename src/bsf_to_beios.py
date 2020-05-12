ec94498706a9d41a95af8d10fe63fefbc3f831c7
Andrii Garkavyi <andrew.gawy@gmail.com>
2020-05-12 08:16:46 +0300

    Logic converting files with ner annotations from BSF (Brown-uk) to BEIOS format

--- src/bsf_to_beios.py
+++ src/bsf_to_beios.py
@@ -1,10 +1,16 @@
 import os
+import glob
 from collections import namedtuple
-from itertools import islice
 import re
+import logging
+from tqdm import tqdm
 
 
 BsfInfo = namedtuple('BsfInfo', 'id, tag, start_idx, end_idx, token')
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
 
 def convert_bsf_2_beios(data: str, bsf_markup: str) -> str:
     """
@@ -34,7 +40,7 @@ def convert_bsf_2_beios(data: str, bsf_markup: str) -> str:
             res.append(m_ln.token + ' S-' + m_ln.tag)
         else:
             res.append(t_words[0] + ' B-' + m_ln.tag)
-            for t_word in islice(t_words, 1, len(t_words) - 1):
+            for t_word in t_words[1: -1]:
                 res.append(t_word + ' I-' + m_ln.tag)
             res.append(t_words[-1] + ' E-' + m_ln.tag)
         prev_idx = m_ln.end_idx
@@ -61,3 +67,40 @@ def parse_bsf(bsf_data: str) -> list:
         bsf = BsfInfo(m.group(1), m.group(2), int(m.group(3)), int(m.group(4)), m.group(5).strip())
         result.append(bsf)
     return result
+
+
+def convert_bsf_to_beios_in_folder(src_dir_path: str, dst_dir_path: str) -> None:
+    ann_path = os.path.join(src_dir_path, '*.tok.ann')
+    ann_files = glob.glob(ann_path)
+    ann_files.sort()
+
+    tok_path = os.path.join(src_dir_path, '*.tok.txt')
+    tok_files = glob.glob(tok_path)
+    tok_files.sort()
+
+    if len(ann_files) == 0 or len(tok_files) == 0:
+        log.warning(f'Token and annotation files are not found at specified path {ann_path}')
+    if len(ann_files) != len(tok_files):
+        log.warning(f'Mismatch between Annotation and Token files. Ann files: {len(ann_files)}, token files: {len(tok_files)}')
+
+    log.info(f'Found {len(tok_files)} files')
+    for (tok_fname, ann_fname) in tqdm(zip(tok_files, ann_files), total=len(tok_files), unit='file'):
+        if tok_fname[:-3] != ann_fname[:-3]:
+            tqdm.write(f'Token and Annotation file names do not match ann={ann_fname}, tok={tok_fname}')
+            continue
+        f_name = '-'.join(tok_fname.split('/')[-1].split('.')[:-2])
+        bio_path = os.path.join(dst_dir_path, f_name + '.bio')
+        with open(tok_fname) as tok_file, open(ann_fname) as ann_file, open(bio_path, 'w') as bio_file:
+            token_data = tok_file.read()
+            ann_data = ann_file.read()
+            beios_data = convert_bsf_2_beios(token_data, ann_data)
+            # log.info(tok_fname)
+            # log.info(beios_data)
+
+            bio_file.write(beios_data)
+
+    log.info('All done')
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    convert_bsf_to_beios_in_folder('/Users/andrew/Projects/law/ner-uk/data/', '/Users/andrew/Projects/law/stanza-training/ner-base/')
