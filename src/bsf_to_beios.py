02ba97fcb43c5b9601a1982e8576a2e55f01d2bb
Andrii Garkavyi <andrew.gawy@gmail.com>
2020-05-12 12:21:59 +0300

    Function to convert BSF notation to BEIOS

--- src/bsf_to_beios.py
+++ src/bsf_to_beios.py
@@ -1,9 +1,10 @@
 import os
 from collections import namedtuple
+from itertools import islice
 import re
 
 
-BsfMeta = namedtuple('BsfMeta', 'id, type, start_idx, end_idx, token')
+BsfInfo = namedtuple('BsfInfo', 'id, tag, start_idx, end_idx, token')
 
 def convert_bsf_2_beios(data: str, bsf_markup: str) -> str:
     """
@@ -14,8 +15,34 @@ def convert_bsf_2_beios(data: str, bsf_markup: str) -> str:
     :return: data in BEIOS format https://en.wikipedia.org/wiki/Inside–outside–beginning_(tagging)
     """
 
+    def join_simple_chunk(chunk: str) -> list:
+        if len(chunk.strip()) == 0:
+            return []
+        tokens = re.split(r'\s', chunk.strip())
+        return [token + ' O' if len(token.strip()) > 0 else token for token in tokens]
 
-    return ''
+    res = []
+    markup = parse_bsf(bsf_markup)
+
+    prev_idx = 0
+    m_ln: BsfInfo
+    for m_ln in markup:
+        res += join_simple_chunk(data[prev_idx:m_ln.start_idx])
+
+        t_words = m_ln.token.split(' ')
+        if len(t_words) == 1:
+            res.append(m_ln.token + ' S-' + m_ln.tag)
+        else:
+            res.append(t_words[0] + ' B-' + m_ln.tag)
+            for t_word in islice(t_words, 1, len(t_words) - 1):
+                res.append(t_word + ' I-' + m_ln.tag)
+            res.append(t_words[-1] + ' E-' + m_ln.tag)
+        prev_idx = m_ln.end_idx
+
+    if prev_idx < len(data) - 1:
+        res += join_simple_chunk(data[prev_idx:])
+
+    return '\n'.join(res)
 
 
 def parse_bsf(bsf_data: str) -> list:
@@ -31,6 +58,6 @@ def parse_bsf(bsf_data: str) -> list:
     ln_ptrn = re.compile(r'(T\d+)\s(\w+)\s(\d+)\s(\d+)\s(.+?)(?=T\d+\s\w+\s\d+\s\d+|$)', flags=re.DOTALL)
     result = []
     for m in ln_ptrn.finditer(bsf_data.strip()):
-        bsf = BsfMeta(m.group(1), m.group(2), int(m.group(3)), int(m.group(4)), m.group(5).strip())
+        bsf = BsfInfo(m.group(1), m.group(2), int(m.group(3)), int(m.group(4)), m.group(5).strip())
         result.append(bsf)
     return result
